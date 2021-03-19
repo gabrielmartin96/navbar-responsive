3befa008ad7b0ba27b7c5aefef006c2063e35f47
KevinKarahali <kevin.karahali@hotmail.com>
2021-03-19 19:47:08 +0100

    Refactoring the navigation bar implementing best practices

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
+window.onload = () => {
+    const navSlide = () => {
+        const burger = document.querySelector('#burger');
+        const nav = document.querySelector('#navbar');
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-    });
-}
+        burger.addEventListener('click', () => {
+            nav.classList.toggle('navbar-active');
+        });
+    };
 
-navSlide();
+    navSlide();
+};
